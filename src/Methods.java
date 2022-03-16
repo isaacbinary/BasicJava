b8920ea156551e12b2ffd754ec74d7b4f454c2c2
HIMANSHU RAWAT <himanshurawat199995@gmail.com>
2022-03-16 15:07:01 +0530

    first java prog commit

---
+++ src/Methods.java
@@ -0,0 +1,19 @@
+
+/* public class Methods {
+
+	public void ValidateHeader() {
+		
+		System.out.println("Header link validated");
+	}
+
+} */
+
+ public class Methods {
+
+	public int ValidateHeader() {
+		
+		System.out.println("Header link validated");
+		return 2; //no need to use if type is void
+	}
+
+}
---
+++ src/Parent.java
@@ -0,0 +1,22 @@
+
+public class Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("First Java....1");
+		//System.out.println("Second Java....2");
+		
+	//	int a = 2;
+	//	int b = 3;
+	//	int sum = a+b;
+	//	System.out.println(sum);
+	//	System.out.println("Sum is "+ sum);
+		
+		System.out.println("I navigated to homepage using Program 2");
+		
+		Methods m = new Methods();
+		//m.ValidateHeader(); // if method is of void type and we are not returning anything
+		System.out.println(m.ValidateHeader()); //if method is of any type (String, int etc.)
+	}
+
+}
